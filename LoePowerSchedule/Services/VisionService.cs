2e9c967005a15238c09479287e2afe4df7c8acf1
Jurkash <jurkash@gmail.com>
2024-12-06 23:47:54 +0200

    OCR issues fixes

--- LoePowerSchedule/Services/VisionService.cs
+++ LoePowerSchedule/Services/VisionService.cs
@@ -132,7 +132,8 @@ public class VisionService(
         using var image = await DownloadImageAsync(imageUrl);
         image.Mutate(imageProcessingContext => imageProcessingContext
             .GaussianSharpen()
-            .Contrast(1.5f)
+            .Contrast(2f)
+            .Saturate(2f)
             .Resize(image.Width * 2, image.Height * 2));
         var customBase64 = image.ToBase64String(JpegFormat.Instance);
         var prefix = "data:image/jpeg;base64,";
